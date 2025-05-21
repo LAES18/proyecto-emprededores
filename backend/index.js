8ebf0907d01754c6ab56a3e7323656cc47f37cc9
LAES18 <nosd407@gmail.com>
2025-05-21 17:18:28 -0600

    intento6

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 53929;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
